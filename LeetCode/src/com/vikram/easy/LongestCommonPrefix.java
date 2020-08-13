4bd95b9c7e2fd2a65282809e58dccb044b48381e
vikramb <vikramb@vikramb-macbookpro3.t-mobile.com>
2020-08-13 16:42:40 -0700

    Improved execution time

--- LeetCode/src/com/vikram/easy/LongestCommonPrefix.java
+++ LeetCode/src/com/vikram/easy/LongestCommonPrefix.java
@@ -4,22 +4,20 @@ public class LongestCommonPrefix {
 
 	public static void main(String[] args) {
 		String [] input = {"flower","flow","flight"};
-		char [] result = new char[input[0].length()];
-		//StringBuilder result = new StringBuilder("");
-		int match = 0;
-		for (int i = 0; i < input[0].length(); i++) {
-			for (int j = 1; j < input.length; j++) {
-				if (input[j].length() == i || input[j].charAt(i) != input[0].charAt(i)) {
-					break;
-				}
-				if (j == input.length -1)
-					match = 1;
-			}
-			if (match == 0) break;
-			result[i] = (input[0].charAt(i));
-			match = 0;
-		}
-		System.out.println(String.valueOf(result));
+		
+		System.out.println(longestCommonPrefix(input));
 	}
+	
+	public static String longestCommonPrefix(String[] strs) {
+		if (strs == null || strs.length == 0) return "";
+	    for (int i = 0; i < strs[0].length() ; i++){
+	        char c = strs[0].charAt(i);
+	        for (int j = 1; j < strs.length; j ++) {
+	            if (i == strs[j].length() || strs[j].charAt(i) != c)
+	                return strs[0].substring(0, i);             
+	        }
+	    }
+	    return strs[0];
+    }
 
 }
